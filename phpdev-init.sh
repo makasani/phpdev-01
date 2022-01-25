337dd847817bcefb78e0fba4ed37c343e7ae3948
makasani <makasani4@yandex.ru>
2022-01-25 17:39:17 +0200

    Homework#2

--- phpdev-init.sh
+++ phpdev-init.sh
@@ -1,10 +1,10 @@
 
 #!/bin/zsh
 
-#apt update
-#apt install curl git php-fpm nginx mysql-server phpmyadmin memcached redis-server php-redis php-memcached
+apt update
+apt install curl git php-fpm nginx mysql-server phpmyadmin memcached redis-server php-redis php-memcached
 
-#wget --output-document=/tmp/vsc.deb  https://code.visualstudio.com/sha/download\?build\=stable\&os\=linux-deb-x64 && dpkg -i  /tmp/vsc.deb 
+wget --output-document=/tmp/vsc.deb  https://code.visualstudio.com/sha/download\?build\=stable\&os\=linux-deb-x64 && dpkg -i  /tmp/vsc.deb 
 
 
 
